0902f5af334899ad949dbf9874bf596e7948dc15
Benjamin Zinszer <bzinszer@gmail.com>
2016-12-05 11:06:23 -0500

    added plot_tiled_data.m
    plot_tiled_data.m takes an n-by-t matrix and plots each column (t) in a different subplot tile. This is useful when plotting multiple channels from one participant or multiple participants from one sample, if you don't want the data to crowd in a single graph. The tiles are supposed to be optimally distributed based on the dimensions of your monitor, but no guarantees!
    
    plot_tiled_data takes arguments:
    - data_array (the n-by-t matrix)
    - type (plot, dot, bar, hist, or imagesc)
    - titletext (a string that will be enumerated for each subplot title, e.g., "Subject #")
    - xlabtext (a string that will appear below each x-axis, e.g., "Time")

---
+++ plot_tiled_data.m
@@ -0,0 +1,33 @@
+function plot_tiled_data(data_array,type,titletext,xlabtext)
+
+%% Configure for plotting the channels
+display_dims = get(0,'MonitorPosition');
+num_panels = size(data_array,ndims(data_array));
+
+vert_tiles = ceil(sqrt(num_panels / (display_dims(3)/display_dims(4))));
+horiz_tiles = ceil(num_panels / vert_tiles);
+
+
+switch type
+    case 'bar'
+        myplot=@(plotdat) bar(plotdat);
+    case 'dot'
+        myplot=@(plotdat) plot(plotdat,'o');
+    case 'hist'
+        myplot=@(plotdat) hist(plotdat);
+    case 'imagesc'
+        myplot=@(plotdat) imagesc(plotdat); colormap('hot');
+    otherwise
+        myplot=@(plotdat) plot(plotdat);
+end
+
+% Iterate over the last dimension of the data_array
+for curr_panel = 1:size(data_array,ndims(data_array));
+    subplot(vert_tiles,horiz_tiles,curr_panel)
+    not_last_dims = repmat({':'},1,ndims(data_array)-1);
+    myplot(data_array(not_last_dims{:},curr_panel))
+    title(sprintf('%s #%g',titletext,curr_panel))
+    xlabel(xlabtext)
+    %ylim([min(data_array(:)) max(data_array(:))]);
+    %xlim([0 size(data_array,1)]);
+end
